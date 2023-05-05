6e2fbee82a3f59ae57ebc046fdc01546f05913e2
Tiago <101526815+369tiago@users.noreply.github.com>
2023-05-05 17:09:50 -0300

    working  😀👍

--- main.c
+++ main.c
@@ -218,7 +218,7 @@ int main(){
 	char opcao;
 	int chave;
 	while (!feof(stdin)){
-		scanf("%c %d", &opcao, &chave);
+		scanf(" %c %d", &opcao, &chave);
 		switch (opcao){
 			case 'i':
 				n = inclui_folha (n, chave);
@@ -226,6 +226,9 @@ int main(){
 			case 'r':
 				n = exclui (n, chave);
 			break;
+			default:
+				fprintf (stderr, "Argumento inválido\n");
+				exit(1);
 		}
 	}
 	emordem (n, n);
