7aa0610bbbf6b4b82adef0ba35d73ce73b0b6ea9
en-22 <132026305+en-22@users.noreply.github.com>
2023-05-07 14:35:53 -0300

    Adição de comentários

--- AVLTree.h
+++ AVLTree.h
@@ -3,33 +3,19 @@ struct no{
 	int altura;
 	struct no *esq, *dir, *pai;
 };
-
+//Cria um nó da árvore com a chave dada, altura = 0, e com os ponteiros para esq, dir e pai iguais a NULL.
 struct no *cria_no (int chave);
-
-int nivelNo (struct no* raiz, int chave);
-
-int maior (int a, int b);
-
-int altura (struct no *no);
-
-int fatorDeBalanceamento (struct no *no);
-
-struct no *rot_esq (struct no *n);
-
-struct no *rot_dir (struct no *n);
-
-struct no *max (struct no *no);
-
-void ajustaPai (struct no *no, struct no *novo);
-
-struct no *balanceia (struct no *n);
-
-struct no *inclui_folha (struct no *n, int chave);
-
-struct no *exclui (struct no *n, int chave);
-
-void imprime_arvore (struct no *n, int esp);
-
-void emordem (struct no *n, struct no *raiz);
-
+//Libera a memória alocada pelos nós da árvore.
 void destroiArvore(struct no *n);
+//Retorna o nível no qual está o no da chave dada.
+int nivelNo (struct no* raiz, int chave);
+//Retorna a altura do nó dado a partir de seu valor na struct no.
+int altura (struct no *no);
+//Inclui um novo nó com a chave dada, respeitando as regras da árvore AVL, podendo rebalancear a árvore se necessário.
+struct no *inclui (struct no *n, int chave);
+//Exclui um nó a partir da chave dada, respeitando as regras da árvore AVL, podendo rebalancear a árvore se necessário.
+struct no *exclui (struct no *n, int chave);
+//Imprime os nós no formato da árvore. Para depuração.
+void imprime_arvore (struct no *n, int esp);
+//Imprime as chaves dos nós em ordem crescente e seus níveis, no formato "chave, nível\n".
+void emordem (struct no *n, struct no *raiz);
